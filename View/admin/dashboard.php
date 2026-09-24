f9681d7c1a4c5033d831b11f8b82aa7a16d1051c
Hafiz <mhafiz0241@gmail.com>
2026-09-24 15:19:58 +0800

    first commit MiniProject2

---
+++ View/admin/dashboard.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ms">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard Admin - Sistem Pendaftaran Kelab</title>
+    
+    
+    <link rel="stylesheet" href="../../assets/css/bootstrap.min.css">
+</head>
+<body>
+
+   
+    <div class="container mt-5">
+        <h1 class="text-primary">Selamat Datang ke Dashboard Admin</h1>
+        <button class="btn btn-success">Tambah Kelab Baru</button>
+    </div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistem Pendaftaran Kelab</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="bg-light">
+    <div class ="container text-center mt-5">
+        <div class = "card shadow p-5 mx-auto" style="max-width: 600px;">
+            <h1 classs = "text-primary mb-3">Sistem Pendaftaran Kelab</h1>
+            <p class="lead text-muted mb-4">Selamat datang! Sila Log masuk ke dalam sistem pendaftaran atau pengurusan kelab.</p>
+
+            <div class = "d-grid gap-2 col-6 mx-auto">
+                <a href="View/auth/login.php" class="btn-dark btn-lg">Log Masuk</a>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
