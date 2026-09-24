8566b18133b92531f98f105ba97f1672280f9dea
mhafiz0241-byte <mhafiz0241@gmail.com>
2026-09-24 21:09:48 +0800

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 </head>
 <body class="bg-light">
     <div class ="container text-center mt-5">
-        <div class = "card shadow p-5 mx-auto" style="max-width: 600px;">
+        <div class = "card shadow p-5 mx-auto" style="max-width: 600 px;">
             <h1 class = "text-primary mb-3">Sistem Pendaftaran Kelab</h1>
             <p class="lead text-muted mb-4">Selamat datang! Sila Log masuk ke dalam sistem pendaftaran atau pengurusan kelab.</p>
 
