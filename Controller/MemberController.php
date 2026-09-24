2a99b27e71485db59a69069735d1047076de383c
Hafiz <mhafiz0241@gmail.com>
2026-09-24 22:08:41 +0800

    update sikit kod

--- Controller/MemberController.php
+++ Controller/MemberController.php
@@ -4,7 +4,7 @@ require_once '../config/conn.php';
 require_once '../models/Club.php';
 
 if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'student') {
-    header("Location: ../views/auth/login.php?error=" . urlencode("Sila log masuk sebagai pelajar terlebih dahulu."));
+    header("Location: ../views/auth/login.php?error=" . urlencode("Please log in as a student first."));
     exit();
 }
 
@@ -16,15 +16,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $club_id = trim($_POST['club_id']); 
 
         if (empty($club_id)) {
-            header("Location: ../views/student/register_club.php?error=" . urlencode("Sila pilih salah satu kelab!"));
+            header("Location: ../views/student/register_club.php?error=" . urlencode("Please choose one club only!"));
             exit();
         }
-        
+
         if ($clubModel->registerStudentToClub($user_id, $club_id)) {
-            header("Location: ../views/student/my_registrations.php?success=" . urlencode("Tahniah! Pendaftaran kelab berjaya."));
+            header("Location: ../views/student/my_registrations.php?success=" . urlencode("Club registered succesfully!"));
             exit();
         } else {
-            header("Location: ../views/student/register_club.php?error=" . urlencode("Gagal mendaftar kelab atau anda sudah mendaftar kelab ini."));
+            header("Location: ../views/student/register_club.php?error=" . urlencode("Failed to register the club or you haved joined this club."));
             exit();
         }
     }
@@ -34,10 +34,10 @@ if ($action === 'cancel') {
     $registration_id = $_GET['id'] ?? null;
 
     if ($registration_id && $clubModel->cancelRegistration($registration_id)) {
-        header("Location: ../views/student/my_registrations.php?success=" . urlencode("Pendaftaran kelab berjaya dibatalkan."));
+        header("Location: ../views/student/my_registrations.php?success=" . urlencode("Your club registered has been canceled"));
         exit();
     } else {
-        header("Location: ../views/student/my_registrations.php?error=" . urlencode("Gagal membatalkan pendaftaran."));
+        header("Location: ../views/student/my_registrations.php?error=" . urlencode("Failed to cancel the register."));
         exit();
     }
 }
