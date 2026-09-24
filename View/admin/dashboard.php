6ac288140ac5f1fabb96a72fa6b228cd1587a78f
Hafiz <mhafiz0241@gmail.com>
2026-09-24 21:05:54 +0800

    update sikit kod

--- View/admin/dashboard.php
+++ View/admin/dashboard.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard Admin - Sistem Pendaftaran Kelab</title>
+    <title>Admin - Sistem Pendaftaran Kelab</title>
     
     
     <link rel="stylesheet" href="../../assets/css/bootstrap.min.css">
